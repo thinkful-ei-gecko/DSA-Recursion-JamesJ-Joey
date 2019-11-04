9820754d96c8ee0e2caa82b1fad60cf60c5cbff9
jamesjenkinsjr <jamesjenkinsjr@icloud.com>
2019-11-04 11:52:10 -0500

    exercise 1

--- exercise.js
+++ exercise.js
@@ -1,1 +1,19 @@
-// 
+// Write a recursive function that counts how many sheep jump over the fence. Your program should take a number as input. That number should be the number of sheep you have. The function should display the number along with the message "Another sheep jumps over the fence" until no more sheep are left.
+
+// Input: 3
+// Output:
+// 3: Another sheep jumps over the fence
+// 2: Another sheep jumps over the fence
+// 1: Another sheep jumps over the fence
+// All sheep jumped over the fence
+
+function countSheep(number) {
+  if (number === 0) {
+    console.log('All sheep jumped over the fence')
+  } else {
+    console.log(`${number}: Another sheep jumps over the fence`)
+    countSheep(number - 1)
+  }
+}
+
+countSheep(3)
